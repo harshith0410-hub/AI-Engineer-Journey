05079cf1ccd02bb4b2d90e69201db9483b9a6f77
G Harshith <gharshith0410@gmail.com>
2026-08-05 13:39:08 +0530

    Added Student Information System

---
+++ student_information.py
@@ -0,0 +1,12 @@
+print("===== Student Information System =====")
+
+name = input("Enter your name: ")
+usn = input("Enter your USN: ")
+branch = input("Enter your branch: ")
+semester = input("Enter your semester: ")
+
+print("\n----- Student Details -----")
+print("Name:", name)
+print("USN:", usn)
+print("Branch:", branch)
+print("Semester:", semester)
